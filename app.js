4ffe3c9c36e19cc3e6217434dce1e5b8d3beee75
Stephan Fowler <stephanfowler@gmail.com>
2012-02-24 00:35:33 +0000

    Grabs individual articles and saves to mongo only if not already saved

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+	var feeds = [
+		'http://www.thesun.co.uk/sol/homepage/feeds/iPad/breaking_news/?iPadApp=true',
+		'http://www.thesun.co.uk/sol/homepage/feeds/iPad/news/?iPadApp=true',
+		'http://www.thesun.co.uk/sol/homepage/feeds/iPad/showbiz/?iPadApp=true',
+	];
+
+	var edition = "20120224";
 
 	var express = require('express'),
 		mongoose = require('mongoose'),
@@ -28,7 +35,7 @@
 	});
 
 	// Mongo setup
-	mongoose.connect('mongodb://localhost/thesun01');
+	mongoose.connect( 'mongodb://localhost/thesun' + edition );
 	Schema = mongoose.Schema; 
 	var ArticleSchema = new Schema({
 		uri             : { type: String, index: { unique: true } }, 
@@ -37,52 +44,115 @@
 		sectionheadline : String,
 		tickerheadline  : String,
 		teaser          : String,
-		teaserImg       : String
+		teaserImg       : String,
+		byline          : String,
+		timestamp       : String,
+		articlebody     : String,
+		image          : [AttachmentSchema],
+		video          : [AttachmentSchema],
+		article        : [AttachmentSchema],
 	});
+	var AttachmentSchema = new Schema({
+		caption         : String,
+		uri             : String
+	});
+
 	var MyArticleModel = mongoose.model( 'ArticleModel', ArticleSchema );
 
 	// HTTP client setup
 	var client = http.createClient(80, 'www.thesun.co.uk');
-	var request = client.request('GET', '/sol/homepage/feeds/iPad/news/?iPadApp=true', {'host': 'www.thesun.co.uk'});
 
-	// Get the feeds...
-	request.on('response', function (response) {
-
-		var data = '';
-
-		response.on('data', function(chunk){ 
-			data += chunk; 
-		});
-
-		response.on('end', function(){
-
-			var parserIndex = new xml2js.Parser('UTF-8');
-			parserIndex.on('end', function(json) {
-				for ( i in json.article ) {
-					var a = json.article[i];
-					var am = new MyArticleModel();
-					am.uri             = conv.convert(a.uri);
-					am.section         = conv.convert(json.section);
-					am.title           = conv.convert(a.title);
-					am.sectionheadline = conv.convert(a.sectionheadline);
-					am.tickerheadline  = conv.convert(a.tickerheadline);
-					am.teaser          = conv.convert(a.teaser);
-					am.teaserImg       = conv.convert(a.teaserImg);
-					console.log(am.uri);
-					am.save(function (err){});
-				}
-			});
-			parserIndex.parseString(data);
-
-		});
-
-	});
+	var getHost = function(url) {
+		var m = url.match(/http:\/\/([^\/]+).*/);
+		if ( m[1] ) {
+			return m[1]
+		}
+	};
+	var getPath = function(url) {
+		var m = url.match(/http:\/\/[^\/]+(.*)/);
+		if ( m[1] ) {
+			return m[1]
+		}
+	};
 	
-	request.end();
+	// Grab the feeds...
+	var articles = {};
 
-	app.get('/', function(req, res){
+	var getXmlAsJson = function( url, callback ) {
+		if ( getHost(url) && getPath(url) ) {
+			var request = client.request('GET', getPath(url), {'host': getHost(url)});
+			request.on('response', function (response) {
+				var data = '';
+				response.on('data', function(chunk){ 
+					data += chunk; 
+				});
+				response.on('end', function(){
+					var parser = new xml2js.Parser('UTF-8');
+					parser.on('end', function( json ) {
+						callback( url, json ) 
+					});
+					parser.parseString(data);
+				});
+			});
+			request.end();
+		};
+	};
+
+	var parseIndexFeed = function( url, json ) {
+		for ( i in json.article ) {
+			var a = json.article[i];
+			console.log( 'CHECKING: ' + a.uri );
+			var parseIfNew = function( a ) {
+				MyArticleModel.count( { uri: a.uri }, function( err, result ){
+					if ( result ) {	
+						console.log( 'EXISTS ALREADY: ' + result + ' ' + a.uri );
+					}
+					else {
+						console.log( 'FETCHING: ' + result + ' ' + a.uri );
+						var am = new MyArticleModel();
+						am.uri             = conv.convert(a.uri);
+						am.section         = conv.convert(json.section);
+						am.title           = conv.convert(a.title);
+						am.sectionheadline = conv.convert(a.sectionheadline);
+						am.tickerheadline  = conv.convert(a.tickerheadline);
+						am.teaser          = conv.convert(a.teaser);
+						am.teaserImg       = conv.convert(a.teaserImg);
+						articles[a.uri]    = am;
+						getXmlAsJson( a.uri, parseArticleFeed );
+					}
+				});
+			}
+			parseIfNew( a );
+		}
+	};
+
+	var parseArticleFeed = function( url, json ) {
+		if ( json.article_content ) {
+			var a = json.article_content;
+			articles[url].byline      = conv.convert(a.byline); 
+			articles[url].timestamp   = conv.convert(a.timestamp); 
+			articles[url].articlebody = conv.convert(a.articlebody); 
+
+			for ( i in a.attachments.attachment ) {
+				var at = a.attachments.attachment[i];
+				var attachment = { caption: conv.convert(at.caption), uri: conv.convert(at.uri) } ;
+				if ( typeof articles[url][at.type] == 'undefined' ) {
+					articles[url][at.type] = [];
+				};
+				articles[url][at.type].push( attachment );
+			}
+			articles[url].save(function (err){});
+			console.log( "\n\nSAVED : " + articles[url].uri );
+		}
+	};
+
+	for ( i in feeds ) {
+		getXmlAsJson( feeds[i], parseIndexFeed );
+	};
+
+    app.get('/', function(req, res){
+		res.end('Updated Edition: ' + edition );
 	});
 
 	app.listen(8080);
-	console.log("Listening on port %d in %s mode", app.address().port, app.settings.env);
-	
+
